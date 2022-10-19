8719db5fd089795e0a7f3e757ccaf2064218a644
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-10-19 15:24:46 +0200

    add FXML graphics

--- src/main/java/se/iths/labboration3/labborationjavafx/HelloApplication.java
+++ src/main/java/se/iths/labboration3/labborationjavafx/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 723, 551);
+        stage.setTitle("Paint-2.0");
         stage.setScene(scene);
         stage.show();
     }
