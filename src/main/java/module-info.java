85459fd9a7c1347d335eec8b00f1aac7c068667b
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-10-21 14:52:17 +0200

    refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module se.iths.labboration3.labborationjavafx {
 
     opens se.iths.labboration3.labborationjavafx to javafx.fxml;
     exports se.iths.labboration3.labborationjavafx;
+    exports se.iths.labboration3.labborationjavafx.controller;
+    opens se.iths.labboration3.labborationjavafx.controller to javafx.fxml;
 }
--- src/main/java/se/iths/labboration3/labborationjavafx/controller/HelloController.java
+++ src/main/java/se/iths/labboration3/labborationjavafx/controller/HelloController.java
@@ -1,9 +1,11 @@
-package se.iths.labboration3.labborationjavafx;
+package se.iths.labboration3.labborationjavafx.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 
 public class HelloController {
+    public MenuButton loadSave;
     @FXML
     private Label welcomeText;
 
