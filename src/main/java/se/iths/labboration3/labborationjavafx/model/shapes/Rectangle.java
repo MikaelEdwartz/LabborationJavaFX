674fe16d311c6d02637622c6a0a4f9b4339e5119
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-10-28 13:55:42 +0200

    refactor collision methods in Rectangle.Java

--- src/main/java/se/iths/labboration3/labborationjavafx/model/shapes/Rectangle.java
+++ src/main/java/se/iths/labboration3/labborationjavafx/model/shapes/Rectangle.java
@@ -10,16 +10,15 @@ public class Rectangle extends Shape {
         super(color, coordinates, size);
     }
 
-    public Rectangle(Color color, double x, double y, double size) {
-        super(color, x, y, size);
-    }
+
 
 
     @Override
     public void draw(GraphicsContext context) {
         double size = getSize();
-        double x = getX() - ((size)/2)*1.75;
-        double y = getY() - size/2;
+        double x = getX() - size / 2 * 1.75;
+        double y = getY() - size / 2 ;
+
         context.setFill(getColor());
         context.fillRect(x, y, size * 1.75, size);
     }
@@ -27,19 +26,23 @@ public class Rectangle extends Shape {
     @Override
     public boolean isInside(Point mouseCoordinate) {
 
-        double leftX = getX() - (getSize()/2)*1.75;
-        double topY = getY() - getSize()/2;
+        double leftXBorder = getX() - getSize() / 2 * 1.75;
+        double rightXBorder = leftXBorder + getSize() * 1.75;
+        double topYBorder = getY() - getSize() / 2;
+        double bottomYBorder = topYBorder + getSize();
+
         double mouseX = mouseCoordinate.x();
         double mouseY = mouseCoordinate.y();
 
-        return mouseX >= leftX &&
-                mouseX <= leftX + (getSize())*1.75 &&
-                mouseY >= topY &&
-                mouseY <= topY + getSize();
+        return mouseX >= leftXBorder &&
+                mouseX <= rightXBorder &&
+                mouseY >= topYBorder &&
+                mouseY <= bottomYBorder;
 
 
 
     }
+
 
 
     @Override
