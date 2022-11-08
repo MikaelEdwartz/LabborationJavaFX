cd941257725cfb2d8286c93dc9187f1ddb5c8f61
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-11-08 18:56:23 +0100

    refactor PaintModel.java

--- src/main/java/se/iths/labboration3/labborationjavafx/model/PaintModel.java
+++ src/main/java/se/iths/labboration3/labborationjavafx/model/PaintModel.java
@@ -10,7 +10,6 @@ import se.iths.labboration3.labborationjavafx.model.Enums.ChangeOption;
 import se.iths.labboration3.labborationjavafx.model.Enums.SelectedShape;
 import se.iths.labboration3.labborationjavafx.model.shapes.Shape;
 import se.iths.labboration3.labborationjavafx.model.shapes.ShapeFactory;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,26 +18,25 @@ public class PaintModel {
     private final ObservableList<Shape> shapes;
     private final ObjectProperty<Color> colorPicker;
     private final StringProperty size;
-    private SelectedShape selectedShape;
     private final List<Shape> changeList;
     private final List<List<Shape>> undoList = new ArrayList<>();
     private final List<List<Shape>> redoList = new ArrayList<>();
-    private final Server server;
-
     private final StringProperty connectToServer;
+    private final Server server;
+    private SelectedShape selectedShape;
 
     public PaintModel() {
-        this.selectorOption = new SimpleBooleanProperty(false);
-        this.colorPicker = new SimpleObjectProperty<>(Color.BLACK);
-        this.size = new SimpleStringProperty("50");
-        this.shapes = FXCollections.observableArrayList(PaintModel::getShapeAttribute);
-        this.changeList = new ArrayList<>();
-        this.selectedShape = SelectedShape.RECTANGLE;
+        selectorOption = new SimpleBooleanProperty(false);
+        colorPicker = new SimpleObjectProperty<>(Color.BLACK);
+        size = new SimpleStringProperty("50");
+        shapes = FXCollections.observableArrayList(PaintModel::getObservableAttributes);
+        changeList = new ArrayList<>();
+        selectedShape = SelectedShape.RECTANGLE;
         server = new Server();
         connectToServer = new SimpleStringProperty("Connect To Server");
     }
 
-    private static Observable[] getShapeAttribute(Shape shape) {
+    private static Observable[] getObservableAttributes(Shape shape) {
         return new Observable[]{
                 shape.colorProperty(),
                 shape.xProperty(),
@@ -48,6 +46,52 @@ public class PaintModel {
         };
     }
 
+    public void setSelectedShape(SelectedShape selectedShape) {
+        switch (selectedShape) {
+            case CIRCLE -> setCircleShape();
+            case RECTANGLE -> setRectangleShape();
+        }
+    }
+
+    public void setCircleShape() {
+        selectedShape = SelectedShape.CIRCLE;
+        setSelectionMode(false);
+    }
+
+    public void setRectangleShape() {
+        selectedShape = SelectedShape.RECTANGLE;
+        setSelectionMode(false);
+    }
+
+    public void setSelectionMode() {
+        selectorOption.set(true);
+    }
+
+    public void checkSelectedShape(Shape shape) {
+        if (isAlreadySelected(shape))
+            removeFromChangeList(shape);
+        else
+            addToChangeList(shape);
+    }
+
+    private boolean isAlreadySelected(Shape shape) {
+        return changeList.contains(shape);
+    }
+
+    private void removeFromChangeList(Shape shape) {
+        shape.setBorderColor(shape.getColor());
+        removeClickedShapeFromList(shape);
+    }
+
+    private void removeClickedShapeFromList(Shape shape) {
+        changeList.remove(shape);
+    }
+
+    private void addToChangeList(Shape shape) {
+        shape.setBorderColor(Color.VIOLET);
+        changeList.add(shape);
+    }
+
     public void checkIfConnectedAndAddToShapes(Shape shape) {
         if (shape == null)
             return;
@@ -55,28 +99,134 @@ public class PaintModel {
         if (server.isConnected())
             server.sendToServer(shape);
         else
-            this.shapes.add(shape);
+            shapes.add(shape);
+    }
+
+    public void addChangesToUndoList() {
+        undoList.add(getTempList());
+    }
+
+    private List<Shape> getTempList() {
+        List<Shape> tempList = new ArrayList<>();
+        getShapes().forEach(shape -> tempList.add(shape.copyOf()));
+        return tempList;
+    }
+
+    public void addToRedoList() {
+        redoList.add(getTempList());
+    }
+
+    public void removeLastChange() {
+        if (undoList.isEmpty())
+            return;
+        undo();
+    }
+
+    private void undo() {
+        shapes.clear();
+        removeLastElementFromList(undoList);
+        revertToListBeforeChange();
+    }
+
+    private void removeLastElementFromList(List<List<Shape>> list) {
+        if (list.size() > 0)
+            list.remove(list.size() - 1);
+    }
+
+    private void revertToListBeforeChange() {
+        addShapesToObservableList(undoList);
+    }
+
+    private void addShapesToObservableList(List<List<Shape>> list) {
+        for (var shape : list.get(list.size() - 1))
+            shapes.add(shape.copyOf());
+    }
+
+    public void revertLastUndo() {
+        if (redoList.isEmpty())
+            return;
+
+        redoLast();
+    }
+
+    private void redoLast() {
+        shapes.clear();
+        addShapesToObservableList(redoList);
+        removeLastElementFromList(redoList);
+    }
+
+    public void changeSelectedShapes(ChangeOption selectedOption) {
+        setChange(selectedOption);
+        setMatchingBorderColor();
+        clearChangeList();
+    }
+
+    private void setChange(ChangeOption selectedOption) {
+        for (var shape : changeList)
+            changeSelectedAttribute(selectedOption, shape);
+    }
+
+    private void changeSelectedAttribute(ChangeOption selectedOption, Shape shape) {
+        switch (selectedOption) {
+            case SIZE -> setNewSize(shape);
+            case COLOR -> setNewColor(shape);
+        }
+    }
+
+    private void setNewSize(Shape shape) {
+        shape.setSize(getSizeAsDouble());
+        sendChangeToServerIfConnected(shape);
+    }
+
+    private void setNewColor(Shape shape) {
+        shape.setColor(getColorPicker());
+        sendChangeToServerIfConnected(shape);
+    }
+
+    private void sendChangeToServerIfConnected(Shape shape) {
+        if (server.isConnected())
+            server.sendToServer(shape);
+    }
+
+    private void setMatchingBorderColor() {
+        for (var shape : changeList)
+            shape.setBorderColor(shape.getColor());
+    }
+
+    public void clearChangeList() {
+        changeList.clear();
+    }
+
+    public void connectToServer() {
+        server.connect(this);
+    }
+
+    public void changeConnectString() {
+        if (server.isConnected())
+            connectToServer.set("Disconnect From Server");
+        else
+            connectToServer.set("Connect To Server");
     }
 
     public void readFromServer(String line) {
-        if (checkIfDrawableShape(line)) return;
+        if (isNotDrawableShape(line)) return;
         addOrChangeShape(line);
     }
 
-    private boolean checkIfDrawableShape(String line) {
+    private boolean isNotDrawableShape(String line) {
         if (isServerMessage(line))
             return true;
 
-        return firstShape(line);
+        return ifFirstShapeAddToList(line);
     }
 
     private static boolean isServerMessage(String line) {
         return line == null || line.contains("joined") || line.contains("left");
     }
 
-    private boolean firstShape(String line) {
+    private boolean ifFirstShapeAddToList(String line) {
         if (getShapes().size() == 0) {
-            this.shapes.add(ShapeFactory.svgToShape(line));
+            shapes.add(ShapeFactory.svgToShape(line));
             return true;
         }
         return false;
@@ -85,7 +235,7 @@ public class PaintModel {
     private void addOrChangeShape(String line) {
         if (shapeExistsChangeInstead(line))
             return;
-        this.shapes.add(ShapeFactory.svgToShape(line));
+        shapes.add(ShapeFactory.svgToShape(line));
     }
 
     private boolean shapeExistsChangeInstead(String line) {
@@ -99,7 +249,7 @@ public class PaintModel {
         return false;
     }
 
-    private static String getCorrectSvgID(String line) {
+    private String getCorrectSvgID(String line) {
         String svgID;
         if (line.contains("[you]"))
             svgID = line.substring(17, 53);
@@ -145,185 +295,15 @@ public class PaintModel {
         return shapes;
     }
 
-    public boolean getSelectorOption() {
-        return selectorOption.get();
-    }
-
-    public void setSelectedShape(SelectedShape selectedShape) {
-        switch (selectedShape) {
-            case CIRCLE -> setCircleShape();
-            case RECTANGLE -> setRectangleShape();
-        }
-    }
-
-    public void setCircleShape() {
-        selectedShape = SelectedShape.CIRCLE;
-        setSelectionMode(false);
-    }
-
-    public void setRectangleShape() {
-        selectedShape = SelectedShape.RECTANGLE;
-        setSelectionMode(false);
-    }
-
-    public void setSelectionMode() {
-        this.selectorOption.set(true);
-    }
-
     public void setSelectionMode(boolean option) {
-        this.selectorOption.set(option);
+        selectorOption.set(option);
     }
 
     public BooleanProperty selectorOptionProperty() {
         return selectorOption;
     }
 
-    public void removeLastChange() {
-        if (undoList.isEmpty())
-            return;
-        undoChange();
-    }
-
-    private void undoChange() {
-        addToRedoList();
-        shapes.clear();
-        removeLastElementFromList(undoList);
-        revertToListBeforeChange();
-    }
-
-    private List<Shape> getTempList() {
-        List<Shape> tempList = new ArrayList<>();
-        getShapes().forEach(shape -> tempList.add(shape.copyOf()));
-        return tempList;
-    }
-
-    private void removeLastElementFromList(List<List<Shape>> list) {
-        if (list.size() > 0)
-            list.remove(list.size() - 1);
-    }
-
-    private void revertToListBeforeChange() {
-        addShapesToShapeList(undoList);
-    }
-
-    private void addShapesToShapeList(List<List<Shape>> list) {
-        for (var shape : list.get(list.size() - 1))
-            shapes.add(shape.copyOf());
-    }
-
-    public void addChangesToUndoList() {
-        undoList.add(getTempList());
-    }
-
-    private void addToRedoList() {
-        redoList.add(getTempList());
-    }
-
-    public void revertLastUndo() {
-        if (redoList.isEmpty())
-            return;
-
-        redoLast();
-    }
-
-    private void redoLast() {
-        shapes.clear();
-        addShapesToShapeList(redoList);
-        removeLastElementFromList(redoList);
-    }
-
-    public void checkIfSelectedAndAddOrRemove(Shape shape) {
-        if (alreadySelected(shape))
-            removeFromChangeList(shape);
-        else
-            addToChangeList(shape);
-    }
-
-    private boolean alreadySelected(Shape shape) {
-        return this.changeList.contains(shape);
-    }
-
-    private void removeFromChangeList(Shape shape) {
-        shape.setBorderColor(shape.getColor());
-        removeClickedShapeFromList(shape);
-    }
-
-    private void removeClickedShapeFromList(Shape shape) {
-        this.changeList.remove(shape);
-    }
-
-    private void addToChangeList(Shape shape) {
-        shape.setBorderColor(Color.VIOLET);
-        this.changeList.add(shape);
-    }
-
-    public void clearChangeList() {
-        this.changeList.clear();
-    }
-
-    public void changeSelectedShapes(ChangeOption selectedOption) {
-        setChange(selectedOption);
-        setMatchingBorderColor();
-        clearChangeList();
-    }
-
-    private void setChange(ChangeOption selectedOption) {
-        for (var shape : this.changeList)
-            changeSelectedAttribute(selectedOption, shape);
-    }
-
-    private void changeSelectedAttribute(ChangeOption selectedOption, Shape shape) {
-        switch (selectedOption) {
-            case SIZE -> setNewSize(shape);
-            case COLOR -> setNewColor(shape);
-        }
-    }
-
-    private void setNewColor(Shape shape) {
-        shape.setColor(getColorPicker());
-        sendChangeToServerIfConnected(shape);
-    }
-
-    private void sendChangeToServerIfConnected(Shape shape) {
-        if (server.isConnected())
-            server.sendToServer(shape);
-    }
-
-    private void setNewSize(Shape shape) {
-        shape.setSize(getSizeAsDouble());
-        sendChangeToServerIfConnected(shape);
-    }
-
-    private void setMatchingBorderColor() {
-        for (var shape : this.changeList)
-            shape.setBorderColor(shape.getColor());
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void connectToServer() {
-        server.connect(this);
-    }
-
-    public String getConnectToServer() {
-        return connectToServer.get();
-    }
-
     public StringProperty connectToServerProperty() {
         return connectToServer;
     }
-
-    public void setConnectToServer(String connectToServer) {
-        this.connectToServer.set(connectToServer);
-    }
-
-    public void changeConnectionString() {
-        if (server.isConnected())
-            connectToServer.set("Disconnect From Server");
-        else
-            connectToServer.set("Connect To Server");
-    }
-
 }
