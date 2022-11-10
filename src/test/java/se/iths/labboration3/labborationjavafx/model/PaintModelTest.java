38374044fd2c07b74ed028f38c05a930a5676d0b
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-11-10 09:20:23 +0100

    add more tests

--- src/test/java/se/iths/labboration3/labborationjavafx/model/PaintModelTest.java
+++ src/test/java/se/iths/labboration3/labborationjavafx/model/PaintModelTest.java
@@ -2,6 +2,7 @@ package se.iths.labboration3.labborationjavafx.model;
 
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
+import se.iths.labboration3.labborationjavafx.model.Enums.ChangeOption;
 import se.iths.labboration3.labborationjavafx.model.Enums.SelectedShape;
 import se.iths.labboration3.labborationjavafx.model.shapes.Rectangle;
 
@@ -24,12 +25,31 @@ class PaintModelTest {
     }
 
     @Test
-    void addToShapesIncreasesShapeListSizeBy1(){
+    void addToShapesIncreasesShapeListSizeBy1() {
         var oldShapeSize = paintModel.getShapes().size();
-        paintModel.getShapes().add(new Rectangle(Color.RED,new Point(1,1), 1, SelectedShape.RECTANGLE));
+        paintModel.getShapes().add(new Rectangle(Color.RED, new Point(1, 1), 1, SelectedShape.RECTANGLE));
         var newShapeSize = paintModel.getShapes().size();
         assertEquals(newShapeSize, (oldShapeSize + 1));
     }
 
+    @Test
+    void callingChangeSizeMethodsChangesSize() {
+        var shape = new Rectangle(Color.RED, new Point(1, 1), 1, SelectedShape.RECTANGLE);
+        paintModel.checkSelectedShape(shape);
+
+        assertEquals(shape.getSize(), 1);
+        paintModel.changeSelectedShapes(ChangeOption.SIZE);
+        assertEquals(shape.getSize(), 50);
+    }
+
+    @Test
+    void callingChangeColorMethodsChangesColor() {
+        var shape = new Rectangle(Color.RED, new Point(1, 1), 1, SelectedShape.RECTANGLE);
+        paintModel.checkSelectedShape(shape);
+
+        assertEquals(shape.getColor(), Color.RED);
+        paintModel.changeSelectedShapes(ChangeOption.COLOR);
+        assertEquals(shape.getColor(), Color.BLACK);
+    }
 
 }
